e995fcff20f3191c24b6f33cc53d2a4634cb5810
Jeffrey Lanters <jeffrey@hulan.nl>
2018-02-18 14:49:18 +0100

    Added opacity to shadow

--- library/PropertyHandlers/Shadow.js
+++ library/PropertyHandlers/Shadow.js
@@ -18,7 +18,18 @@ var Shadow = function () {
   _createClass(Shadow, [{
     key: "parse",
     value: function parse(property) {
-      return { "box-shadow": "0px 0px " + property.value + " rgba(0,0,0,0.2)" };
+      var _split = property.value.split(" ");
+      switch (_split.length) {
+        case 3:
+          var _alpha = _split[2].replace("px", "");
+          return {
+            "box-shadow": "0px 0px " + _split[0] + " " + _split[1] + " rgba(0,0,0," + _alpha + ")"
+          };
+        default:
+          return {
+            "box-shadow": "0px 0px " + property.value + " rgba(0,0,0,0.2)"
+          };
+      }
     }
   }]);
 
